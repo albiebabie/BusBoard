0fdd876e046fadbff27ae798e893b12de09b8515
Andy Achilleos <andreas.achilleos@endclothing.com>
2018-04-27 16:22:04 +0100

    Can print closest 5 bus arrivals

---
+++ index.js
@@ -0,0 +1,37 @@
+const readlineSync = require("readline-sync");
+const moment = require("moment");
+
+// Your program should ask the user for a stop code,...
+//  and print a list of the next five buses at that stop code, with their routes, destinations, and the
+// time until they arrive in minutes.
+const busStopCode = "490008660N";
+// let stopCode = readlineSync.question("Please input a stop code: ");
+// console.log("You have entered bus stop code: " + stopCode);
+
+var request = require("request");
+request(
+  "https://api.tfl.gov.uk/StopPoint/" +
+    busStopCode +
+    "/Arrivals?app_id=e6d22ef6&app_key=d52e914b030019b478891bd9a6a964f6",
+  function(error, response, body) {
+    const listOfBusArrivals = JSON.parse(body);
+    const sortedBusArrivals = getBusArrivalsByClosest(listOfBusArrivals);
+    printNextFiveBuses(sortedBusArrivals);
+  }
+);
+
+function printNextFiveBuses(listOfBusArrivals) {
+  for (i = 0; i < Math.min(listOfBusArrivals.length, 5); i++) {
+    const busArrival = listOfBusArrivals[i];
+    console.log(i + 1);
+    console.log("************************************************** \n");
+    console.log(busArrival);
+  }
+}
+
+function getBusArrivalsByClosest(listOfBusArrivals) {
+  listOfBusArrivals.sort(function(a, b) {
+    return moment(b.expectedArrival).isBefore(a.expectedArrival);
+  });
+  return listOfBusArrivals;
+}
